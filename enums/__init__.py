ae89602587531bce1e5851967545986f74a7c529
LucasVolp <lucasgabrielvmb@gmail.com>
2025-05-09 18:01:20 -0400

    feat: adiciona enums TypeEvent e Status, e atualiza imports nos modelos

---
+++ enums/__init__.py
@@ -0,0 +1,7 @@
+from .typeEvent import TypeEvent
+from .status import Status
+
+__all__ = [
+    "TypeEvent",
+    "Status"
+]
---
+++ enums/status.py
@@ -0,0 +1,8 @@
+# types/status.py
+from enum import Enum
+
+class Status(Enum):
+    ACTIVE = "ACTIVE"
+    INACTIVE = "INACTIVE"
+    DELETED = "DELETED"
+    CANCELLED = "CANCELLED"
